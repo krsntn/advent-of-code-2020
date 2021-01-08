fe7355083533d5816bdd346c1ae33b6a6c10b859
krsntn <kanahan0110@gmail.com>
2021-01-08 12:38:39 +0800

    day 3

---
+++ src/day-03/puzzle-01.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const filename = './input.txt';
+const input = fs
+  .readFileSync(filename, 'utf8')
+  .split('\n')
+  .map((text) => text);
+
+let trees = 0;
+
+function moveDown(input, pos) {
+  const position = pos % input.length;
+
+  if (input[position] === '#') {
+    trees++;
+  }
+}
+
+for (let index = 1; index < input.length; index++) {
+  const nextElement = input[index];
+  moveDown(nextElement, index * 3);
+}
+
+// output answer
+console.log(trees);
---
+++ src/day-03/puzzle-02.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const filename = './input.txt';
+const input = fs
+  .readFileSync(filename, 'utf8')
+  .split('\n')
+  .map((text) => text);
+
+function meetTree(input, pos) {
+  const position = pos % input.length;
+  return input[position] === '#';
+}
+
+function go(right, down) {
+  let trees = 0;
+  for (let i = 1, index = down; index < input.length; i++, index += down) {
+    const element = input[index];
+    meetTree(element, i * right) && trees++;
+  }
+  return trees;
+}
+
+// input
+const inputList = [
+  [1, 1],
+  [3, 1],
+  [5, 1],
+  [7, 1],
+  [1, 2],
+];
+
+const output = [];
+inputList.forEach((item) => {
+  output.push(go(item[0], item[1]));
+});
+
+// output answer
+console.log(output.reduce((a, b) => a * b));
